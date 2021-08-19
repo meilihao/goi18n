d509b6a7e1140281f8f7fc65cdb92613038e405f
meilihao <563278383@qq.com>
2021-08-19 14:58:58 +0800

    + examples

---
+++ examples/internal/i18n/i18n.go
@@ -0,0 +1,40 @@
+package i18n
+
+import (
+	"github.com/meilihao/goi18n/v2"
+)
+
+var (
+	Arg = struct {
+		Mobile *goi18n.Elem
+		Parse  *goi18n.Elem
+	}{
+
+		Mobile: &goi18n.Elem{
+			Key: "Arg.Mobile",
+			Map: map[string]string{
+				"zh": `{0}长度不等于11位或{1}格式错误!`,
+				"en": `{0} length need 11 or {1} format invalid!`,
+			},
+		},
+		Parse: &goi18n.Elem{
+			Key: "Arg.Parse",
+			Map: map[string]string{
+				"zh": `%s`,
+				"en": `%s`,
+			},
+		},
+	}
+	Token = struct {
+		Empty *goi18n.Elem
+	}{
+
+		Empty: &goi18n.Elem{
+			Key: "Token.Empty",
+			Map: map[string]string{
+				"zh": `token为空`,
+				"en": `token empty`,
+			},
+		},
+	}
+)
